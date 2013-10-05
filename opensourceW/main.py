f6e2870add56712bc7e11bf95a6b9bbb3fc7853f
MeenaGuptaIwasaki <mguptaiwasaki@hotmail.com>
2013-10-05 15:57:00 -0400

    With commenting and a test function

--- opensourceW/main.py
+++ opensourceW/main.py
@@ -1,5 +1,21 @@
-from OC import OC
+#Code from open source hack workshop
+#Modified By Meena Gupta-Iwasaki
+#To do stuff
 
+
+from OC import OC
+import unittest
+
+#acquire the articles from OC
 articles = OC.articles.list()
 
-print articles['32']
+#a function to print a return elements under '32 in a dictionary
+def print32(articles):
+    print articles['32']    #print the values under '32' in a dictionary
+    return articles['32']; #return the values under '32' in a dictionary
+
+#Attempt to test the function, but broken right now.
+def testprint32(articles):
+    unittest.assert(print32(articles) == articles['32'])
+
+print32(articles)
